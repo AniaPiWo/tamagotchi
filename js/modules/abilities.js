83f226077a4252007ca181c1fb624a66f3a5f88d
AniaPiWo <snowflake@wp.pl>
2023-03-05 14:34:24 +0100

    states

--- js/modules/abilities.js
+++ js/modules/abilities.js
@@ -1,5 +1,18 @@
 export default class Abilities {
-  constructor() {
+  constructor({ characterState }) {
     console.log("Abilities module initialized");
+
+    const buttons = [
+      { class: ".play", state: "PLAYING" },
+      { class: ".eat", state: "EATING" },
+      { class: ".sleep", state: "SLEEPING" },
+    ];
+
+    buttons.forEach(({ class: buttonClass, state }) => {
+      const button = document.querySelector(buttonClass);
+      button.addEventListener("mousedown", () => {
+        characterState.innerText = state;
+      });
+    });
   }
 }
--- js/modules/tamagotchi.js
+++ js/modules/tamagotchi.js
@@ -1,9 +1,11 @@
 export default class Tamagotchi {
-  constructor() {
+  constructor({ characterState, characterImage }) {
     this.health = { value: 10, importance: 1, element: null };
     this.hunger = { value: 10, importance: 3, element: null };
     this.energy = { value: 10, importance: 2, element: null };
     this.fun = { value: 10, importance: 4, element: null };
+    this.characterState = characterState;
+    this.characterImage = characterImage;
     console.log("Tamagotchi initialized");
 
     this.healthDecrease = setInterval(() => {
@@ -11,19 +13,19 @@ export default class Tamagotchi {
         this.health.value -= 1;
         this.displayHealth(this.health.element);
         if (this.health.value <= 0) {
-          console.log("💀");
           clearInterval(this.healthDecrease);
         }
       }
+      this.updateState();
     }, 1000);
 
     this.hungerDecrease = setInterval(() => {
       this.hunger.value -= 1;
       this.displayHunger(this.hunger.element);
       if (this.hunger.value <= 0) {
-        console.log("💀");
         clearInterval(this.hungerDecrease);
       }
+      this.updateState();
     }, 1000);
 
     this.energyDecrease = setInterval(() => {
@@ -37,20 +39,60 @@ export default class Tamagotchi {
       }
       this.displayEnergy(this.energy.element);
       if (this.energy.value <= 0) {
-        console.log("💀");
         clearInterval(this.energyDecrease);
       }
+      this.updateState();
     }, 2000);
 
     this.funDecrease = setInterval(() => {
       this.fun.value -= 1;
       this.displayFun(this.fun.element);
       if (this.fun.value <= 0) {
-        console.log("💀");
         clearInterval(this.funDecrease);
       }
+      this.updateState();
     }, 1000);
   }
+
+  updateState = () => {
+    let state = "";
+    let imgPath = "";
+    if (this.health.value <= 0) {
+      state = "DEAD";
+      imgPath = "./img/NimoMoods/State=Dead.png";
+      clearInterval(this.healthDecrease);
+    } else if (this.energy.value <= 6) {
+      state = "TIRED";
+      imgPath = "./img/NimoMoods/State=Sleepy.png";
+    } else if (this.hunger.value <= 6) {
+      state = "HUNGRY";
+      imgPath = "./img/NimoMoods/State=Hungry.png";
+    } else if (this.fun.value <= 6) {
+      state = "BORED";
+      imgPath = "./img/NimoMoods/State=Bored.png";
+    } else {
+      state = "HAPPY";
+      imgPath = "./img/NimoMoods/State=Standing.png";
+    }
+
+    if (state !== this.currentState) {
+      console.log(state);
+      if (this.characterState) {
+        this.characterState.innerText = state;
+      }
+
+      if (this.characterImage) {
+        this.characterImage.src = imgPath;
+      }
+
+      this.currentState = state;
+    }
+
+    this.displayHealth(this.health.element);
+    this.displayHunger(this.hunger.element);
+    this.displayEnergy(this.energy.element);
+    this.displayFun(this.fun.element);
+  };
 
   displayHealth = (elementSelector) => {
     const displayElement = document.querySelector(elementSelector);
