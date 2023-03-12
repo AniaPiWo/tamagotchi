bbc1019209285d0a11a2c0023d8cecd450c950b8
AniaPiWo <snowflake@wp.pl>
2023-03-12 12:13:58 +0100

    eating not working

--- js/modules/abilities.js
+++ js/modules/abilities.js
@@ -1,5 +1,14 @@
 export default class Abilities {
-  constructor({ characterState }) {
+  constructor(tamagotchi, characterState, characterImage, hunger, health) {
+    this.tamagotchi = tamagotchi;
+    this.isEating = false;
+    this.isSleeping = false;
+    this.isPlaying = false;
+    this.characterState = characterState;
+    this.characterImage = characterImage;
+    this.hunger = hunger;
+    this.health = health;
+    this.eatingInterval = null;
     console.log("Abilities module initialized");
 
     const buttons = [
@@ -8,11 +17,52 @@ export default class Abilities {
       { class: ".sleep", state: "SLEEPING" },
     ];
 
-    buttons.forEach(({ class: buttonClass, state }) => {
+    buttons.forEach(({ class: buttonClass }) => {
       const button = document.querySelector(buttonClass);
-      button.addEventListener("mousedown", () => {
-        characterState.innerText = state;
-      });
+      if (button.classList.contains("eat")) {
+        button.addEventListener("click", this.toggleEating);
+      } else if (button.classList.contains("sleep")) {
+        button.addEventListener("click", this.startSleeping);
+      } else if (button.classList.contains("play")) {
+        button.addEventListener("click", this.startPlaying);
+      }
     });
   }
+
+  startPlaying() {
+    console.log("Tamagotchi:", this.tamagotchi);
+  }
+
+  startEating = () => {
+    this.eatingInterval = setInterval(() => {
+      this.hunger.value += 2;
+      this.displayHunger(this.hunger.element);
+      if (this.health.value <= 0) {
+        clearInterval(this.eatingInterval);
+        this.isEating = false;
+      }
+      if (!this.isEating) {
+        clearInterval(this.eatingInterval);
+      }
+    }, 1000);
+  };
+
+  stopEating = () => {
+    clearInterval(this.eatingInterval);
+    this.isEating = false;
+  };
+
+  toggleEating = () => {
+    this.isEating = !this.isEating;
+    if (this.isEating) {
+      this.startEating();
+      console.log("start eating");
+      this.characterState.innerText = "EATING";
+      this.characterImage.src = "./img/NimoMoods/State=Eating1.png";
+    } else {
+      this.stopEating();
+      console.log("stop eating");
+      this.characterImage.src = "./img/NimoMoods/State=Standing.png";
+    }
+  };
 }
